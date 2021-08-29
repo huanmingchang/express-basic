4bf1cfaff6ebea3b06d182944a744a2b29c3ba75
huanmingchang <huanming1988@gmail.com>
2021-08-29 09:08:10 +0800

    feat: implement bootstrap

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('project init')
+  res.render('index')
 })
 
 app.listen(3000, () => {
