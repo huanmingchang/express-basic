84fceb1cc3451f78f3ab0dadd2934991b295bc76
huanmingchang <huanming1988@gmail.com>
2021-08-29 09:55:21 +0800

    feat: add about page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
